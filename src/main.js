1abda49107c916e55b7df638285df331126f84b7
aouali <aghiles.ouali@nassidj.com>
2023-04-29 20:57:23 +0100

    page de listing des composants

--- src/main.js
+++ src/main.js
@@ -1,16 +1,67 @@
-import "./style.css";
-import Alpine from "alpinejs";
+import './style.css'
+import './libs/components'
+import data from './libs/data'
+import Alpine from 'alpinejs'
 
 //import collapse from '@alpinejs/collapse'
-document.addEventListener("alpine:init", () => {
-  Alpine.data("dropdown", () => ({
-    open: false,
-    toggle() {
-      this.open = !this.open;
-    },
-  }));
-});
-//Alpine.plugin(collapse)
-Alpine.start();
+document.addEventListener('alpine:init', () => {
+    Alpine.data('dropdown', () => ({
+      open: false,
+      toggle () {
+          this.open = !this.open
+        }
+    }))
+    Alpine.data('store',()=>({
+      dataStore: null,
+      getStoreSections() {
+        return this.dataStore = data().components.marketing.sections
+      },
+      getStoreElements() {
+        return this.dataStore = data().components.marketing.elements
+      },
+      getStoreFeedback() {
+        return this.dataStore = data().components.marketing.feedback
+      },
+      getStorePageExemple() {
+        return this.dataStore = data().components.marketing.pageExamples
+      },
+      getStoreApplicationShells() {
+        return this.dataStore = data().components.applUI.applicationShells
+      },
+      getStoreDisplayData() {
+        return this.dataStore = data().components.applUI.displayData
+      },
+      getStoreElements() {
+        return this.dataStore = data().components.applUI.elements
+      },
+      getStoreFeedback() {
+        return this.dataStore = data().components.applUI.feedback
+      },
+      getStoreForms() {
+        return this.dataStore = data().components.applUI.forms
+      },
+      getStoreHeadings() {
+        return this.dataStore = data().components.applUI.headings
+      },
+      getStoreLayouts() {
+        return this.dataStore = data().components.applUI.layouts
+      },
+      getStoreLists() {
+        return this.dataStore = data().components.applUI.Lists
+      },
+      getStoreNavigations() {
+        return this.dataStore = data().components.applUI.navigations
+      },
+      getStoreOverlays() {
+        return this.dataStore = data().components.applUI.overlay
+      },
+      getStorePageExempleUi() {
+        return this.dataStore = data().components.applUI.pageExemple
+      },
 
-window.alpine = Alpine;
+
+    }))
+})
+//Alpine.plugin(collapse)
+Alpine.start()
+window.alpine = Alpine
